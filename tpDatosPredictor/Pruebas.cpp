8b2a9b91ad853d6114ebfde9e5575190ea6af791
fito25rnr@gmail.com <fito25rnr@gmail.com@47f48a9c-75d6-ec65-ca2c-1c50deb39040>
2014-10-13 18:58:53 +0000

    agrego metodo para probar ngramas

--- tpDatosPredictor/Pruebas.cpp
+++ tpDatosPredictor/Pruebas.cpp
@@ -7,11 +7,17 @@
 
 #include "parser/ArbolB.h"
 #include "parser/Nodo.h"
+#include "NGrama.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <utility>
+#include <algorithm>    // std::stable_sort
 
 using namespace std;
 
@@ -28,10 +34,31 @@ void PruebaArbol() {
 	cout<<"Fin de pruebas Arbol."<<endl;
 }
 
+void pruebaNgrama(){
+	string oracion ="This is a very beautiful day. This is a very big house. The elephant is very big. The house is small. The dog is black. The car is very big and red.";
+
+	//oracion.max long ngrama y separador.
+	NGrama ngrama5 =  NGrama(oracion, 5,",");
+	ngrama5.stringANgrama();
+
+
+
+	vector<pair<string,int> > listaNgrama;
+	listaNgrama = ngrama5.getListaNgrama();
+    std::stable_sort(listaNgrama.begin(), listaNgrama.end());//,compare_first_only());
+    std::cout << std::endl << "Sorted:" << std::endl;
+
+	for(std::size_t i=0;ngrama5.getListaNgrama().size();++i){
+		cout << listaNgrama[i].first  << "," << listaNgrama[i].second << endl;
+		//cout << ngrama5.getListaNgrama()[i] << endl;
+
+	}
+}
 
 int main(int argc, char *argv[]){
 	//aca voy llamando a las diferentes pruebas
 
-	PruebaArbol();
+	//PruebaArbol();
+	pruebaNgrama();
 
 }
