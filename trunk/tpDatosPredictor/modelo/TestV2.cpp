746ed1a67208b510ce0c85ff456f3300dc692fa3
fito25rnr <fito25rnr@47f48a9c-75d6-ec65-ca2c-1c50deb39040>
2014-10-19 19:54:29 +0000

    clase encargada de almacenar un registro (linea) del archivo test_v2.txt, su id, su sentencia y su lista de ngramas

---
+++ trunk/tpDatosPredictor/modelo/TestV2.cpp
@@ -0,0 +1,32 @@
+/*
+ * TestV2.cpp
+ *
+ *  Created on: 19/10/2014
+ *      Author: lucas
+ */
+
+#include "TestV2.h"
+
+
+namespace std {
+
+TestV2::TestV2() {
+	this->id = 0;
+	this->sentente = "";
+	this->sentencePredicha = "";
+
+}
+
+TestV2::TestV2(int id, string sentence,vector<pair<string,int> > listaNgrama) {
+	this->id = id;
+	this->sentente = sentence;
+	this->listaNgrama = listaNgrama;
+	this->sentencePredicha = "";
+
+}
+
+TestV2::~TestV2() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ trunk/tpDatosPredictor/modelo/TestV2.h
@@ -0,0 +1,64 @@
+/*
+ * TestV2.h
+ *
+ *  Created on: 19/10/2014
+ *      Author: lucas
+ */
+
+#ifndef TESTV2_H_
+#define TESTV2_H_
+#include <string>
+#include <vector>
+
+namespace std {
+
+class TestV2 {
+
+
+private:
+ 	int id;
+	string sentente; //contiene una sentencia incompleta.
+	string sentencePredicha; //contiene la sentencia completa.
+	vector<pair<string,int> > listaNgrama;
+
+public:
+	TestV2();
+	TestV2(int id, string sentence,vector<pair<string,int> > listaNgrama);
+	virtual ~TestV2();
+
+	int getId() const {
+		return id;
+	}
+
+	void setId(int id) {
+		this->id = id;
+	}
+
+	const vector<pair<string, int> >& getListaNgrama() const {
+		return listaNgrama;
+	}
+
+	void setListaNgrama(const vector<pair<string, int> >& listaNgrama) {
+		this->listaNgrama = listaNgrama;
+	}
+
+	const string& getSentente() const {
+		return sentente;
+	}
+
+	void setSentente(const string& sentente) {
+		this->sentente = sentente;
+	}
+
+	const string& getSentencePredicha() const {
+		return sentencePredicha;
+	}
+
+	void setSentencePredicha(const string& sentencePredicha) {
+		this->sentencePredicha = sentencePredicha;
+	}
+};
+
+} /* namespace std */
+
+#endif /* TESTV2_H_ */
