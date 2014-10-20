3da142adbcd471ef0294e396d2b104f63edf6270
pellarolojuani@gmail.com <pellarolojuani@gmail.com@47f48a9c-75d6-ec65-ca2c-1c50deb39040>
2014-10-20 00:23:07 +0000

    se crea la clase Registro para representar cada linea del archivo de texto de ngramas con los atributos contexto, termino y frecuencia para despues poder calcular la probabilidad de un determinado termino en un determinado contexto.

---
+++ trunk/tpDatosPredictor/NGrama/Registro.cpp
@@ -0,0 +1,38 @@
+/*
+ * Registro.cpp
+ *
+ *  Created on: 19/10/2014
+ *      Author: juanignacio
+ */
+
+#include "Registro.h"
+
+Registro::Registro() {
+	this->contexto = "";
+	this->termino = "";
+	this->frecuencia = 0;
+}
+
+Registro::~Registro() {
+	// TODO Auto-generated destructor stub
+}
+
+void Registro::setContexto(string unContexto){
+	this->contexto = unContexto;
+}
+void Registro::setTermino(string unTermino){
+	this->termino = unTermino;
+}
+void Registro::setFrecuencia(int unaFrecuencia){
+	this->frecuencia = unaFrecuencia;
+}
+string Registro::getContexto(){
+	return this->contexto;
+}
+string Registro::getTermino(){
+	return this->termino;
+}
+int Registro::getFrecuencia(){
+	return this->frecuencia;
+}
+
