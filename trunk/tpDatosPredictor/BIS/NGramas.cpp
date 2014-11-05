e4c950911dce42db3249adb824dc4a8f219535ed
pellarolojuani@gmail.com <pellarolojuani@gmail.com@47f48a9c-75d6-ec65-ca2c-1c50deb39040>
2014-11-05 23:13:31 +0000

    algunos avances en el set de pruebas

--- trunk/tpDatosPredictor/BIS/NGramas.cpp
+++ trunk/tpDatosPredictor/BIS/NGramas.cpp
@@ -29,6 +29,38 @@ abb::ArbolB<Registro,60>* NGramas::getLexico(){
 	return this->lexico;
 }
 
+//--------------------------------------------------------//
+//---------Comienzo de funciones auxiliares --------------//
+string getSiguienteTermino(FILE* fp){
+	string termino = "";
+	char c = fgetc(fp);
+	if (c == ' ') c = fgetc(fp);
+	while (c > 32 && c != ' '){ //a partir del ascii 33 comienzan los caracteres que necesitamos
+		termino += c;
+		c = fgetc(fp);
+	}
+	//cout<<termino<<" -- "<<termino.size()<<endl;
+
+	return termino;
+}
+
+string sacarPrimerTermino(string texto){
+	//saca la primera palabra de una frase y devuelve la frase nueva
+	int espacio = texto.find_first_of(" ");
+	string nuevoTexto = texto.substr(espacio+1);
+	return nuevoTexto;
+}
+
+void NGramas::agregarRegistroEnArbol(Registro unRegistro){
+	if (!this->lexico->buscar(unRegistro)){
+		this->lexico->insertar(unRegistro);
+	}else{
+		this->lexico->aumentarFrecuencia(unRegistro);
+	}
+}
+//--------------------------------------------------------//
+//--------------Fin de funciones auxiliares --------------//
+
 void NGramas::streamANgrama(FILE* fp){
 
 	char *c = new char[2684354560];	//reservamos 2.5gb de memoria!
@@ -61,35 +93,3 @@ void NGramas::streamANgrama(FILE* fp){
 	}
 
 }
-
-//--------------------------------------------------------//
-//---------Comienzo de funciones auxiliares --------------//
-string getSiguienteTermino(FILE* fp){
-	string termino = "";
-	char c = fgetc(fp);
-	if (c == ' ') c = fgetc(fp);
-	while (c > 32 && c != ' '){ //a partir del ascii 33 comienzan los caracteres que necesitamos
-		termino += c;
-		c = fgetc(fp);
-	}
-	//cout<<termino<<" -- "<<termino.size()<<endl;
-
-	return termino;
-}
-
-string sacarPrimerTermino(string texto){
-	//saca la primera palabra de una frase y devuelve la frase nueva
-	int espacio = texto.find_first_of(" ");
-	string nuevoTexto = texto.substr(espacio+1);
-	return nuevoTexto;
-}
-
-void NGramas::agregarRegistroEnArbol(Registro unRegistro){
-	if (!this->lexico->buscar(unRegistro)){
-		this->lexico->insertar(unRegistro);
-	}else{
-		this->lexico->aumentarFrecuencia(unRegistro);
-	}
-}
-//--------------------------------------------------------//
-//--------------Fin de funciones auxiliares --------------//
