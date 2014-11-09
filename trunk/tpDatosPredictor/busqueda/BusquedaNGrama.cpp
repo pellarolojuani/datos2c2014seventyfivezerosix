8568d7e21c8a134f86158b596e3b244617d11892
fito25rnr <fito25rnr@47f48a9c-75d6-ec65-ca2c-1c50deb39040>
2014-11-09 21:03:06 +0000

    resolucion de consultas a partir busquedas en los map. Falta terminar

--- trunk/tpDatosPredictor/busqueda/BusquedaNGrama.cpp
+++ trunk/tpDatosPredictor/busqueda/BusquedaNGrama.cpp
@@ -6,6 +6,11 @@
  */
 
 #include "BusquedaNGrama.h"
+#include "../NGrama/NGrama.h"
+#include <iostream>
+#include <fstream>
+#include "../modelo/TestV2.h"
+#include <map>
 
 using namespace std;
 
@@ -19,7 +24,7 @@ BusquedaNGrama::~BusquedaNGrama() {
 	// TODO Auto-generated destructor stub
 }
 
-string BusquedaNGrama::buscarPalabraFaltanteAPartirDeNgrama(string ngrama, string pathArchivo){
+string BusquedaNGrama::buscarPalabraFaltanteAPartirDeNgrama(string frase,map<string,int> mapDeNGramasSet){
 
 	/*
 	 * -armo los 4grama con long = 4
@@ -33,7 +38,76 @@ string BusquedaNGrama::buscarPalabraFaltanteAPartirDeNgrama(string ngrama, strin
 	 *  - armo la frase completa con el ngrama de M y luego la devuelvo.
 	 *
 	*/
+	//guardo la frasee original
+	string fraseOriginal = frase;
+	NGrama ngrama3 = NGrama(frase, 3,",");
+	ngrama3.stringANGramaMax();
+	frase.clear();
+	frase = calculoProbabilidadYElijoNgrama(mapDeNGramasSet,ngrama3.getMapDeNGramas());
+	ngrama3.~NGrama();
+	//con el ngrama de 3 seleccionado, armo los de 2.
+	NGrama ngrama2 = NGrama(frase, 2,",");
+	ngrama2.stringANGramaMax();
+	frase = calculoProbabilidadYElijoNgrama(mapDeNGramasSet,ngrama2.getMapDeNGramas());
+	string palabra = buscarPalabra(frase,ngrama2.getMapDeNGramas());
 
+	return armarSentenceCompleta(fraseOriginal,palabra);
+}
+
+void BusquedaNGrama::resolverConsultas(map<string,int> mapNGrama){
+
+		ifstream leer_fich ("test_v2.txt");
+		ofstream fichero("test_v2_completo.txt");
+
+		TestV2 testV2 = TestV2();
+		char* linea = new char[2800];
+		//descarto la sentencia 1;
+		leer_fich.getline(linea,2800,'\n');
+		fichero << linea << endl;
+
+		for (int i = 0; i < 8; i ++){
+
+			leer_fich.getline(linea,2800,'\n');
+			std::string oracionAux = std::string(linea);
+
+			string oracion = oracionAux.erase(oracionAux.find_last_not_of(" \n\r\t")+1);
+			//cout<<oracion<<endl;
+			std::size_t pos = oracion.find(",");
+			string id = oracion.substr(0,pos);
+			string sentence = oracion.substr(pos+1);
+			//cout<<sentence.find('"')<<endl;
+			string sentenceSinComilla = sentence.substr(1,sentence.rfind('"')-1);
+			testV2.setSentenceSinComillas(sentenceSinComilla);
+			testV2.setId(std::atoi(id.c_str()));
+			testV2.setSentence(sentence);
+
+			cout<<"---"<<endl;
+			cout<<"ID: "<<testV2.getId()<<endl;
+			cout<<"SENTENCE: "<<testV2.getSentence()<<endl;
+			cout<<"SENTENCE SIN COM: "<<testV2.getSentenceSinComillas()<<endl;
+			cout<<"---"<<endl;
+			//aca guardo la sentencia completo en el archivo de salida.
+			fichero << testV2.getId() << ","<<'"'<<testV2.getSentenceSinComillas()<<'"'<<endl;
+
+		}
+
+		for(std::size_t i=0; i < testV2.getListaNgrama().size();++i){
+			cout << testV2.getListaNgrama()[i].first  << "," << testV2.getListaNgrama()[i].second << endl;
+		}
+		leer_fich.close();
+		fichero.close();
+}
+
+string BusquedaNGrama::calculoProbabilidadYElijoNgrama(map<string,int> mapDeNGramasSet,map<string,int> mapDeNGramasSentence){
 	return "";
 }
+string BusquedaNGrama::buscarPalabra(string ngrama,map<string,int> mapDeNGramasSet){
+	return "";
+}
+
+string BusquedaNGrama::armarSentenceCompleta(string fraseOriginal,string palabra){
+	return "";
+}
+
+
 
